e54c38b1ec9d582a22ee1f8f31df4635e65218cd
依小窗 <>
2021-03-10 14:06:52 +0800

    Initial commit

---
+++ example/android/app/src/main/java/com/quyaxinli/screen_shot_listen_plugin_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.quyaxinli.screen_shot_listen_plugin_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/ScreenShotListenPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface ScreenShotListenPlugin : NSObject<FlutterPlugin>
+@end
